590f26cce9d63166a10a8ec3c67d96f06ad37a65
Sebsterio <sebastian.rosloniec@gmail.com>
2020-05-01 18:31:40 +0200

    refactor controls module

--- src/js/controls.js
+++ src/js/controls.js
@@ -2,44 +2,59 @@
 	if (!controlsEnabled) return;
 
 	const controls = document.querySelector(".house__controls");
-	const panel = controls.querySelector(".house__panel");
-	const btnClose = controls.querySelector(".house__btn--close");
-	const btnAdd = controls.querySelector(".house__btn--add");
-	// const btnRemove = controls.querySelector(".house__btn--remove");
+	const mainPanel = controls.querySelector(".house__panel--main");
+	const buttons = controls.querySelectorAll(".house__btn");
 	const newLink = controls.querySelector(".house__new-link");
 
+	// Toggle the whole controls module
 	function toggleControls(on) {
 		if (on) controls.classList.add("house__config--show-controls");
 		else controls.classList.remove("house__config--show-controls");
 	}
 
-	function toggleMenu(on, btn, menu) {
+	// Toggle a panel and corresponding button
+	function toggleMenu(btn, menu, on) {
 		if (on) {
-			btn.classList.add("house__btn--active");
-			menu.classList.remove("house__new-link--hidden");
+			if (btn) btn.classList.add("house__btn--active");
+			if (menu) menu.classList.add("house__panel--active");
 		} else if (on === false) {
-			btn.classList.remove("house__btn--active");
-			menu.classList.add("house__new-link--hidden");
+			if (btn) btn.classList.remove("house__btn--active");
+			if (menu) menu.classList.remove("house__panel--active");
 		} else {
-			btn.classList.toggle("house__btn--active");
-			menu.classList.toggle("house__new-link--hidden");
+			if (btn) btn.classList.toggle("house__btn--active");
+			if (menu) menu.classList.toggle("house__panel--active");
 		}
 	}
 
-	function hideCurrentPanel() {
-		panel
-			.querySelector(".house__btn--active")
-			.classList.remove(".house__btn--active");
+	function handleButtonClick() {
+		const target = this.dataset.target;
+
+		if (target === "close") {
+			toggleControls(false); // hide entire controls module
+			return;
+		}
+
+		const activeBtn = mainPanel.querySelector(".house__btn--active");
+		const activePanel = controls.querySelector(".house__panel--active");
+		const targetPanel = controls.querySelector(".house__panel--" + target);
+
+		// If nothing is open, open clicked elements immediately
+		if (!activeBtn && !activePanel) {
+			toggleMenu(this, targetPanel, true);
+		}
+		// If a menu is open, hide it and open new menu if different than current
+		else {
+			toggleMenu(activeBtn, activePanel, false);
+			if (this !== activeBtn) {
+				setTimeout(() => {
+					toggleMenu(this, targetPanel, true);
+				}, 400);
+			}
+		}
 	}
 
 	// ------------------- init -------------------
 
-	btnAdd.addEventListener("click", () => {
-		toggleMenu(null, btnAdd, newLink);
-	});
-	btnClose.addEventListener("click", () => {
-		toggleControls(false); // Hide controls on clicking btn--close
-	});
-
 	toggleControls(true); // Show controls
+	buttons.forEach((btn) => btn.addEventListener("click", handleButtonClick));
 })(houseConfig.controlsEnabled);
